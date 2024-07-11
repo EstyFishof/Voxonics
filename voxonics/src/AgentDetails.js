3f5ce0012c8b572d5970dc2e177bbfae073997a4
Buch620 <allgoodsk@gmail.com>
2024-07-11 11:14:03 +0300

    delate mui and install again

--- voxonics/src/AgentDetails.js
+++ voxonics/src/AgentDetails.js
@@ -6,7 +6,16 @@ import './AgentDetails.css';
 export default class AgentDetails extends React.Component{
 
     render(){
-        return(<div className="a_details"><img src="propil agent"/>
+        return(<><head>
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+<link
+  rel="stylesheet"
+  href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap"
+/>
+
+        </head>
+        <div className="a_details"><img src="propil agent"/>
     <h3>Hi agent_Name!</h3>
     {/* <h3>Hi {agent_Name}!</h3> */}
     {/* <label value="Agent Number(ID)" /> */}
@@ -26,7 +35,7 @@ export default class AgentDetails extends React.Component{
     
     <input type="button" value="Ready" className="btn_ready"/>
     <input type="button" /*login*//>
-    </div>)
+    </div></>)
 }
 }
 
